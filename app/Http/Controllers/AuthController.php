355b83d9facd3d79cb1d9592455322dc1ec6d90d
WaleedAhmed <you@example.com>
2024-02-03 23:57:20 -0800

    Add your commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function loginPost(Request $request)
+    {
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->mobile = $request->mobile;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+        $credetials = [
+            'name' => $request->name,
+            'mobile' => $request->mobile,
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credetials)) {
+            return redirect('/home')->with('success', 'Login Success');
+        }
+
+        return back()->with('error', 'Error Email or Password');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login');
+    }
+}
---
+++ resources/views/Admin.blade.php
@@ -0,0 +1,7 @@
+<form action="" method="post">
+    @csrf
+    <input type="text" name="name" placeholder="name">
+    <hr>
+    <textarea name="content" placeholder="content"></textarea>
+    <input type="submit" value="Creete">
+</form>
